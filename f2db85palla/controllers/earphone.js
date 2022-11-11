8a8f113289e46e623600710c7fa98d29483b8a9a
VENUREDDY981 <108357564+VENUREDDY981@users.noreply.github.com>
2022-11-11 01:06:56 -0600

    details changed

--- f2db85palla/controllers/earphone.js
+++ f2db85palla/controllers/earphone.js
@@ -14,13 +14,13 @@ exports.earphone_list = async function (req, res) {
 exports.earphone_detail = async function(req, res) {
     console.log("detail" + req.params.id)
     try {
-    result = await earphone.findById( req.params.id)
+    result = await Earphone.findById(req.params.id)
     res.send(result)
     } catch (error) {
     res.status(500)
     res.send(`{"error": document for id ${req.params.id} not found`);
     }
- };
+   };
 
 // Handle Earphone create on POST.
 exports.earphone_create_post = async function (req, res) {
@@ -29,7 +29,7 @@ exports.earphone_create_post = async function (req, res) {
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"earphone_type":"goat", "cost":12, "size":"large"}
+    // {"Name":"boat", "Manufacture_Location":12, "price":"large"}
     document.Name = req.body.Name;
     document.Manufacture_Location = req.body.Manufacture_Location;
     document.price = req.body.price;
@@ -47,9 +47,25 @@ exports.earphone_delete = function (req, res) {
     res.send('NOT IMPLEMENTED: Earphone delete DELETE ' + req.params.id);
 };
 // Handle Earphone update form on PUT.
-exports.earphone_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: Earphone update PUT' + req.params.id);
-};
+exports.earphone_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+   ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await Earphone.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.Name)
+    toUpdate.Name = req.body.Name;
+    if(req.body.Manufacture_Location) toUpdate.Manufacture_Location = req.body.Manufacture_Location;
+    if(req.body.price) toUpdate.price = req.body.price;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+   failed`);
+    }
+   };
 exports.earphone_view_all_Page = async function (req, res) {
     try {
         theEarphones = await Earphone.find();
