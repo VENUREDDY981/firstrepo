1b07a4d2a5abc5bb5a77faf8a355aab6f5b100d7
VENUREDDY981 <108357564+VENUREDDY981@users.noreply.github.com>
2022-11-16 19:36:15 -0600

    details changed

--- f2db85palla/controllers/earphone.js
+++ f2db85palla/controllers/earphone.js
@@ -11,16 +11,16 @@ exports.earphone_list = async function (req, res) {
     }
 }
 // for a specific Earphone.
-exports.earphone_detail = async function(req, res) {
+exports.earphone_detail = async function (req, res) {
     console.log("detail" + req.params.id)
     try {
-    result = await Earphone.findById(req.params.id)
-    res.send(result)
+        result = await Earphone.findById(req.params.id)
+        res.send(result)
     } catch (error) {
-    res.status(500)
-    res.send(`{"error": document for id ${req.params.id} not found`);
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
     }
-   };
+};
 
 // Handle Earphone create on POST.
 exports.earphone_create_post = async function (req, res) {
@@ -42,30 +42,38 @@ exports.earphone_create_post = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
-// Handle Earphone delete form on DELETE.
-exports.earphone_delete = function (req, res) {
-    res.send('NOT IMPLEMENTED: Earphone delete DELETE ' + req.params.id);
+/// Handle Earphone delete on DELETE.
+exports.earphone_delete = async function (req, res) {
+    console.log("delete " + req.params.id)
+    try {
+        result = await Earphone.findByIdAndDelete(req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
 };
 // Handle Earphone update form on PUT.
-exports.earphone_update_put = async function(req, res) {
+exports.earphone_update_put = async function (req, res) {
     console.log(`update on id ${req.params.id} with body
    ${JSON.stringify(req.body)}`)
     try {
-    let toUpdate = await Earphone.findById( req.params.id)
-    // Do updates of properties
-    if(req.body.Name)
-    toUpdate.Name = req.body.Name;
-    if(req.body.Manufacture_Location) toUpdate.Manufacture_Location = req.body.Manufacture_Location;
-    if(req.body.price) toUpdate.price = req.body.price;
-    let result = await toUpdate.save();
-    console.log("Sucess " + result)
-    res.send(result)
+        let toUpdate = await Earphone.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.Name)
+            toUpdate.Name = req.body.Name;
+        if (req.body.Manufacture_Location) toUpdate.Manufacture_Location = req.body.Manufacture_Location;
+        if (req.body.price) toUpdate.price = req.body.price;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
     } catch (err) {
-    res.status(500)
-    res.send(`{"error": ${err}: Update for id ${req.params.id}
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
    failed`);
     }
-   };
+};
 exports.earphone_view_all_Page = async function (req, res) {
     try {
         theEarphones = await Earphone.find();
@@ -77,3 +85,52 @@ exports.earphone_view_all_Page = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
+// Handle a show one view with id specified by query
+exports.earphone_view_one_Page = async function (req, res) {
+    console.log("single view for id " + req.query.id)
+    try {
+        result = await Earphone.findById(req.query.id)
+        res.render('earphonedetail',
+            { title: 'earphone Detail', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+exports.earphone_create_Page = function (req, res) {
+    console.log("create view")
+    try {
+        res.render('earphonecreate', { title: 'Earphone Create' });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+// query provides the id
+exports.earphone_update_Page = async function (req, res) {
+    console.log("update view for item " + req.query.id)
+    try {
+        let result = await Earphone.findById(req.query.id)
+        res.render('earphoneupdate', { title: 'Earphone Update', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+exports.earphone_delete_Page = async function (req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try {
+        result = await Earphone.findById(req.query.id)
+        res.render('earphonedelete', {
+            title: 'Earphone Delete', toShow:
+                result
+        });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+}
--- f2db85palla/routes/earphones.js
+++ f2db85palla/routes/earphones.js
@@ -3,4 +3,9 @@ const earphone_controlers= require('../controllers/earphone');
 var router = express.Router();
 /* GET earphones */
 router.get('/', earphone_controlers.earphone_view_all_Page );
+/* GET detail earphone page */
+router.get('/detail', earphone_controlers.earphone_view_one_Page);
+router.get('/create', earphone_controlers.earphone_create_Page);
+router.get('/update', earphone_controlers.earphone_update_Page);
+router.get('/delete', earphone_controlers.earphone_delete_Page);
 module.exports = router;
